422510ce8bda6e1a4dfc7fb2bdb994aec3df2ccc
ZnCookie <ZnCookie@users.noreply.github.com>
2026-05-20 01:42:34 +0800

    feat: add IRC client module for private message listening

--- osubot-core/src/lib.rs
+++ osubot-core/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod api;
 pub mod commands;
 pub mod highlight;
+pub mod irc;
 pub mod rate_limiter;
 pub mod response;
 pub mod storage;
@@ -11,6 +12,7 @@ pub use commands::parse_command;
 pub use highlight::{
     format_highlight, get_highlight, HighlightError, HighlightResult, UserHighlight,
 };
+pub use irc::IrcConfig;
 pub use rate_limiter::{RateLimitError, RateLimiter};
 pub use response::{format_stats, format_stats_with_change};
 pub use storage::Storage;
